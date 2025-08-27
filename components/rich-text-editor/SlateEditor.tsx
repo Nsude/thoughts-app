ea77c14088b7accaf831111f1041c95ff12f0488
Meshach Nsude <nsudeme777@gmail.com>
2025-08-28 00:08:45 +0100

    implement base logic for creating a new thought

--- components/rich-text-editor/SlateEditor.tsx
+++ components/rich-text-editor/SlateEditor.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { useCallback, useEffect, useMemo, useReducer, useRef, useState } from "react"
 import { createEditor, Descendant, Editor, Element, Transforms } from "slate"
 import { withHistory } from "slate-history"
 import { Editable, RenderElementProps, Slate, withReact } from "slate-react"
@@ -17,18 +17,125 @@ interface Props {
   thoughtId: Id<"thoughts">
 }
 
-export default function SlateEditor({ handleClick, handleValueChange, thoughtId }: Props) {
-  // editor instance 
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
-  const initialValue: Descendant[] = useMemo(() => [
-    {
-      type: "paragraph",
-      children: [{ text: "" }]
-    }], []);
+// central state 
+type EditorState = {
+  status: "idle" | "loading" | "saving" | "error";
+  lastSavedContent: any[] | null;
+  isInitialised: boolean;
+  isCreatingThought: boolean;
+}
 
-  const handleSlateValueChange = useCallback((content: any[]) => {
+// all editor actions
+type EditorAction = 
+| {type: "INIT_CONTENT"}
+| {type: "CONTENT_LOADED", content: any[]}
+| {type: "CREATING_THOUGHT"}
+| {type: "ERROR", msg: any};
+
+const initialState:EditorState = {
+  status: "idle",
+  lastSavedContent: null,
+  isInitialised: false,
+  isCreatingThought: false
+}
+
+// central reducer
+const editorReducer = (state: EditorState, action: EditorAction) => {
+  switch(action.type) {
+    case "INIT_CONTENT":
+      return {
+        ...state,
+        status: "loading",
+        isInitialised: false
+      } as EditorState;
+    case "CREATING_THOUGHT":
+      return {
+        ...state,
+        isCreatingThought: true,
+        isInitialised: false,
+        status: "saving"
+      } as EditorState
+    case "CONTENT_LOADED":
+      return {
+        ...state,
+        status: "idle",
+        lastSavedContent: action.content,
+        isInitialised: true
+      } as EditorState;
+    case "ERROR": 
+      return {
+        ...state,
+        status: 'error'
+      } as EditorState;
+  }
+}
+
+// function to get the length of the charactes on the page not counting spaces
+const getContentLength = (content: any[]):number => {
+  if (!content || content.length === 0) return 0;
+
+  return content.reduce((total, node) => {
+    if (node.children) {
+      return total + (node.children as any[]).reduce((nodeTotal, child) => {
+        return nodeTotal + (child.text?.trim().length || 0)
+      }, 0)
+    }
+    return total;
+  }, 0)
+}
+
+export default function SlateEditor({ handleClick, handleValueChange, thoughtId }: Props) {
+  const [state, dispatch] = useReducer(editorReducer, initialState);
+
+  // editor instance 
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []); 
+  const initialValue: Descendant[] = useMemo(() => [{ type: "paragraph", children: [{ text: "" }]}], []);
+  const router = useRouter();
+
+  // convex mutations
+  const createThought = useMutation(api.thoughts.createNewThought);
+  const createDocument = useMutation(api.thoughts.createNewDocument);
+  const setCoreThought = useMutation(api.thoughts.setCoreThought);
+
+  const handleNewThought = useCallback(async (content: any[]) => {
+    const contentLength = getContentLength(content);
+    if (contentLength < 10) return;
+
+    dispatch({type: "CREATING_THOUGHT"});
+
+    try {
+      const newThoughtId = await createThought({isPrivate: true});
+      if (!newThoughtId) return;
+      // create a document for the thought
+      const coreThought = await createDocument({
+        title: "Untitled Thought",
+        thoughtFileId: newThoughtId,
+        content
+      })
+
+      // init document as the core thought of the created thought
+      await setCoreThought({coreThought: coreThought, thoughtId: newThoughtId});
+      dispatch({type: "CONTENT_LOADED", content});
+      router.replace(`/thoughts/${newThoughtId}`);
+    } catch (error) {
+      console.error("Error creating new thought: ", error);
+      dispatch({type: "ERROR", msg: error});
+    }
+  }, [thoughtId, router])
+
+  const handleSlateValueChange = useCallback(async (content: any[]) => {
+    if (!content) return;
+
     handleValueChange(content); // call external value change
-  }, [])
+
+    if (state.isInitialised || state.isCreatingThought) return;
+
+    if (thoughtId === "new") {
+      await handleNewThought(content);
+    }
+
+
+  }, [handleValueChange, handleNewThought, thoughtId, state.isInitialised, state.isCreatingThought])
 
   // render the selected element type
   const renderElement = useCallback((props: RenderElementProps) => {
