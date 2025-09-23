2c4365b96149d6253371d813f41c8f22cd1ce2f3
Meshach Nsude <nsudeme777@gmail.com>
2025-09-23 15:42:04 +0100

    reroute from home  if user is not authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export default function Home() {
-  redirect("/thoughts/new");
+  const currentUser = useConvexAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (currentUser.isLoading) return;
+    if (!currentUser.isAuthenticated) {
+      return router.replace("/login")
+    }
+    
+    redirect("/thoughts/new");
+  }, [currentUser.isAuthenticated, currentUser.isLoading, router])
+
 }
